8600de32571c2083d6cc62113e25a38c9ffe33c7
Damian-CNC <Damian-CNC@users.noreply.github.com>
2026-09-17 11:27:08 +0000

    deploy: f4ba7619f5c2bcd2e1117bc5994ecc6cf6716870

---
+++ assets/ResultDisplay-Cm7bM5Jo.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-Dyd4WcUp.js";const i=({children:e,className:s=""})=>t.jsx("div",{className:`result-box flex items-center justify-center min-h-[3rem] ${s}`,children:e});export{i as R};
